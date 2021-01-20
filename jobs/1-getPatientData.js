eb072b69f84fe00dc6f2255a48b297260aabad83
lakhassane <mamadou@openfn.org>
2021-01-20 09:16:48 +0000

    mamadou@openfn.org's change on OpenFn.org

--- jobs/1-getPatientData.js
+++ jobs/1-getPatientData.js
@@ -9,16 +9,8 @@ post(
     strictSSL: false,
   },
   state => {
+    console.log(state.data.body.id);
     return { ...state, access_token: state.data.body.id };
   }
 );
 
-get(
-  `${state.configuration.url}/vPatients?access_token=${state.access_token}`,
-  { strictSSL: false },
-  state => {
-    // mappings in hre
-    console.log(state.data);
-    return state;
-  }
-);
