d199642b611d1b8f4f038e6c8e2f540225acc0aa
mamadou <mamadou.lakhassane@gmail.com>
2021-01-20 10:11:12 +0000

    troubleshoot token for second operation, #1

--- jobs/1-getPatientData.js
+++ jobs/1-getPatientData.js
@@ -1,3 +1,4 @@
+// operation 1 is a post, to get an access token
 post(
   `${state.configuration.url}/Users/login`,
   {
@@ -9,18 +10,22 @@ post(
     strictSSL: false,
   },
   state => {
-    console.log(state.data.body.id);
-    return { ...state, access_token: state.data.body.id };
+    const accessToken = state.data.body.id;
+    console.log('Authentication done...');
+    return { ...state, accessToken };
   }
 );
 
+// operation 2 is a get, using the token, to get patients
 get(
-  `${state.configuration.url}/vPatients?access_token=${state.access_token}`,
-  { strictSSL: false },
+  `${state.configuration.url}/vPatients`,
+  {
+    query: state => ({ access_token: state.accessToken }),
+    strictSSL: false
+  },
   state => {
-    // mappings in hre
+    // mappings in here
     console.log(state.data);
     return state;
   }
 );
-
