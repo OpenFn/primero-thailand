fd475e5fb2630f2d255b9c28f5b3c48d5cf5db32
mamadou <mamadou.lakhassane@gmail.com>
2021-01-27 12:50:43 +0000

    update api endpoint, #1

--- jobs/1-getPatientData.js
+++ jobs/1-getPatientData.js
@@ -7,25 +7,29 @@ post(
       email: state.configuration.email,
       password: state.configuration.password,
     },
+    // https: newAgent({ rejectUnauthorized: false }),
     strictSSL: false,
   },
   state => {
-    const accessToken = state.data.body.id;
+    const access_token = state.data.body.id;
     console.log('Authentication done...');
-    return { ...state, accessToken };
-  }
-);
-
-// operation 2 is a get, using the token, to get patients
-get(
-  `${state.configuration.url}/vPatients`,
-  {
-    query: state => ({ access_token: state.accessToken }),
-    strictSSL: false
-  },
-  state => {
-    // mappings in here
-    console.log(state.data);
-    return state;
+    // operation 2 is a get, using the token, to get people
+    filter = {
+      where: { cid: state.references[0].national_id },
+      include: 'interventions',
+      limit: 1,
+    };
+    return get(
+      `${state.configuration.url}/people`,
+      {
+        query: { filter, access_token },
+        // https: newAgent({ rejectUnauthorized: false }),
+        strictSSL: false,
+      },
+      state => {
+        // console.log(state.data);
+        return state;
+      }
+    )(state);
   }
 );
