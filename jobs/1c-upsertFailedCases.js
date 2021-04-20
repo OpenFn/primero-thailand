a30c400b633eedf73f880bc35f0c434ea88b7d8a
lakhassane <mamadou@openfn.org>
2021-04-20 10:35:01 +0000

    mamadou@openfn.org's change on OpenFn.org

--- jobs/1c-upsertFailedCases.js
+++ jobs/1c-upsertFailedCases.js
@@ -1,10 +1,9 @@
 alterState(state => {
-  console.log(state.data);
   const data = {
     mark_synced: true,
     mark_synced_url:
       'https://www.openfn.org/inbox/7b080edf-4466-4041-a4b3-9dbfdf02daee',
-    record_id: state.record_id, //upserting by record_id now
+    record_id: state.data.record_id, //upserting by record_id now
   };
 
   console.log('Upserting case', JSON.stringify(data, null, 2));
