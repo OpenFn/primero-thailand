12bd306026a4f5e5f79d441526faa22172b2a6f3
mamadou <mamadou.lakhassane@gmail.com>
2021-04-29 10:30:31 +0000

    check type of error for status, #4

--- jobs/1c-upsertFailedCases.js
+++ jobs/1c-upsertFailedCases.js
@@ -1,14 +1,21 @@
 alterState(state => {
-  console.log(state.error);
+  const error = state.error;
+
+  let status = 'failed';
+  for (elt of error)
+    if (elt.includes('MODEL_NOT_FOUND')) {
+      status = 'not_found';
+    }
+
   const data = {
     mark_synced: true,
+    mark_synced_status: status,
     mark_synced_url:
       'https://www.openfn.org/inbox/7b080edf-4466-4041-a4b3-9dbfdf02daee',
     record_id: state.data.data.record_id, //upserting by record_id now
   };
 
   console.log('Upserting case', JSON.stringify(data, null, 2));
-  // return state;
   return upsertCase(
     {
       externalIds: ['record_id'],
