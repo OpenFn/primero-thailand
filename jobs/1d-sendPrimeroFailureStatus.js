f92cfb15b6e7212c542c2e422539b50feafa1b6b
Taylor Downs <downs.taylor@gmail.com>
2021-08-04 15:43:04 +0100

    use state.record_id, guessing at the shape of state from BEFORE the failed run, #19

--- jobs/1d-sendPrimeroFailureStatus.js
+++ jobs/1d-sendPrimeroFailureStatus.js
@@ -12,7 +12,7 @@ fn(state => {
     mark_synced_status: status,
     mark_synced_url:
       'https://www.openfn.org/inbox/7b080edf-4466-4041-a4b3-9dbfdf02daee',
-    record_id: state.data.data.record_id, //upserting by record_id now
+    record_id: state.record_id, // upserting by record_id now
   };
 
   console.log('Upserting case', JSON.stringify(data, null, 2));
