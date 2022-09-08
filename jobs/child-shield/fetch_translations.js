367a200ff34761b1f6b59b0c88902dfe998c2d89
mtuchi <mtuchidev@gmail.com>
2022-09-08 20:01:50 +0300

    wip: clean up mapping for desired output

--- jobs/child-shield/fetch_translations.js
+++ jobs/child-shield/fetch_translations.js
@@ -1,292 +1,266 @@
 // get forms from Primero
-get(
-    `${state.configuration.url}/api/v2/forms`,
-    {
-        headers: { 'content-type': 'application/json' },
-        authentication: {
-            username: state.configuration.user,
-            password: state.configuration.password
-        },
-        strictSSL: false,
-    },
-    state => {
-        return state;
-    }
-);
-
-
+get(`${state.configuration.url}/api/v2/forms`, {
+  headers: { 'content-type': 'application/json' },
+  authentication: {
+    username: state.configuration.user,
+    password: state.configuration.password,
+  },
+  strictSSL: false,
+});
 
 // get selected fields in google sheets
 // get(
-//     `${state.configuration.spreedsheetUrl}/${state.configuration.spreedsheetId}/values/Select%20Fields?majorDimension=COLUMNS&valueRenderOption=FORMATTED_VALUE&key=${state.configuration.googleAPI_KEY} `,
+//     `${state.configuration.spreedsheetUrl}/${state.configuration.spreedsheetId}/values/Select%20Fields`,
 //     {
 //         headers: { 'content-type': 'application/json' },
-//         strictSSL: false,
+//         params: {
+//             majorDimension: "COLUMNS",
+//             valueRenderOption: "FORMATTED_VALUE",
+//             key: state.configuration.googleAPI_KEY
+//         },
 //     },
 //     state => {
-//         return { "fields_references": state.value }
+//         const select_fields = state.data
+//         console.log(select_fields)
+//         return state;
 //     }
 // );
 
-// Pluck the option_source_strings
+// Get a list of selected externallyDefinedOptionSets (either as IDs or objects)
 fn(state => {
-    const forms_data = state.data.data
-    // console.log(state.data)
+  const forms = state.data.data;
 
-    // Assume we have the select fields values from Google sheet
-    const select_fields = [
-        "location_current",
-        "occupation_1",
-        "if_out_of_school__specify_reason",
-        "nationality_fa822dc",
-        "gender_4e8704a",
-        "relationship_with_the_abused_eb757fc",
-        "social_status_6c1074a",
-        "occupation_09639f9",
-        "stimulant_9191303",
-        "relation_within_family_41a62f4",
-        "environment_3c4fc2b",
-        "economic_issues_84c5b46",
-        "physical_issue_1f182e4",
-        "mental_issue_5607d21",
-        "relationship_with_the_abused_365fcd1",
-        "gender_2dea3c9",
-        "social_status_7c0989a",
-        "occupation_c4d6420",
-        "location_of_the_incident_885fe8c",
-        "sender_s_gender_b83c931",
-        "sender_s_occupation_3ed671e",
-        "type_of_place_where_the_incident_occurred_bdc967d",
-        "type_of_abuse_431f2ba",
-        "specify_physical_abuse_4867f38",
-        "specify_sexual_abuse_16d36f7",
-        "specify_physical_interaction_d13d273",
-        "sexual_intercourse_a1585d0",
-        "if_no_physical_touch__please_specify_1159dde",
-        "specify_mental_abuse_6182644",
-        "specify_neglect_9b8a22b",
-        "specify_exploitation_ef65c2e",
-        "specify_human_trafficking_dca84dd",
-        "stimulant_a4096c2",
-        "relation_within_family_a6c45fd",
-        "economic_issue_a445217",
-        "physical_issue_2e9f1c6",
-        "mental_issue_4785ece",
-        "urgent_protection_concern",
-        "risk_level",
-        "social_problems_identified_1",
-        "home_visit_required__2",
-        "vagina_details",
-        "cervix_details",
-        "discharge_details",
-        "uterus_details",
-        "penis_details_79f31be",
-        "urethra_and_discharge_60eb64e",
-        "scrotal_sac_f93e04a",
-        "anus_and_rectum_eaf3784",
-        "source_of_information_44cac9a",
-        "disabilities_1",
-        "chronic_illness",
-        "mental_illness",
-        "behavior_of_risk",
-        "developmental_issue",
-        "vaccinations_are_up_to_date",
-        "nutritional_conditions_are_adequate",
-        "caregiver_s_disabilities",
-        "caregiver_s_chronic_illness_es",
-        "caregiver_s_mental_illness_es",
-        "caregiver_s_economic_issue_s",
-        "caregiver_s_substance_abuse_issue",
-        "physical_abuse_issue",
-        "gambling_issue_in_the_family",
-        "substance_abuse_in_the_family_community",
-        "entertainment_venues_near_the_household",
-        "girls_lie_together_in_the_same_room_as_their_male_relatives",
-        "girls_lie_together_in_the_same_room_as_their_female_relatives",
-        "boys_lie_in_the_same_room_as_their_female_relatives",
-        "boys_lie_together_in_the_same_room_as_their_male_relatives",
-        "history_of_illegal_activities_behaviors_in_the_family",
-        "history_of_child_labor_in_the_family_community",
-        "bullying",
-        "mingling_and_causing_annoyance_to_others",
-        "gambling",
-        "substance_abuse",
-        "involved_in_sex_trade",
-        "behaves_in_a_sexually_inappropriate_or_obscene_manner",
-        "resisting_parental_teachings",
-        "family_status",
-        "is_this_person_living_in_the_same_household_as_patient_7d39e1d",
-        "relation",
-        "relation_is_alive",
-        "relation_nationality",
-        "occupation_3",
-        "relation_location_current",
-        "reason_for_unexpected_pregnancy",
-        "specify_contraception_problem",
-        "specify_legal_offenses",
-        "factors_for_pregnancy_termination",
-        "specify_physical_issue",
-        "specify_mental_issue",
-        "specify_family_social_economic_issues",
-        "patient_provided_with_alternative_counseling",
-        "patient_decision_after_being_provided_with_alternative_counseling",
-        "antenatal_care_will_be_provided_at",
-        "support_needed",
-        "support_needed_1",
-        "source_of_information_647b9db",
-        "is_the_subject_given_the_benefit_of_the_doubt_",
-        "assessment_method",
-        "urgent_protection_concern",
-        "protection_concerns",
-        "risk_level",
-        "conference_type",
-        "conference_reason",
-        "conference_case_status",
-        "conference_case_transfer_reason",
-        "relation_to_the_child_37c30dc",
-        "protection_concerns",
-        "sex_check_and_evaluation",
-        "physical",
-        "mental",
-        "social",
-        "physical_1",
-        "mental_1",
-        "reproductive_health",
-        "psycho_social",
-        "status_of_treatment_plan_a8ca0e8",
-        "service_response_type",
-        "service_type",
-        "service_response_timeframe",
-        "service_implementing_agency",
-        "service_implementing_agency_individual",
-        "service_referral",
-        "service_implemented",
-        "service_delivery_location",
-        "status_of_treatment_plan_a8ca0e8",
-        "protection_concerns",
-        "risk_level",
-        "followup_type",
-        "child_was_seen",
-        "reason_child_not_seen",
-        "action_taken_already",
-        "need_follow_up_visit",
-        "recommend_case_closed",
-        "status",
-        "closure_reason"
-    ];
+  // Assume we have the select fields values from Google sheet
+  const select_fields = [
+    'location_current',
+    'occupation_1',
+    'if_out_of_school__specify_reason',
+    'nationality_fa822dc',
+    'gender_4e8704a',
+    'relationship_with_the_abused_eb757fc',
+    'social_status_6c1074a',
+    'occupation_09639f9',
+    'stimulant_9191303',
+    'relation_within_family_41a62f4',
+    'environment_3c4fc2b',
+    'economic_issues_84c5b46',
+    'physical_issue_1f182e4',
+    'mental_issue_5607d21',
+    'relationship_with_the_abused_365fcd1',
+    'gender_2dea3c9',
+    'social_status_7c0989a',
+    'occupation_c4d6420',
+    'location_of_the_incident_885fe8c',
+    'sender_s_gender_b83c931',
+    'sender_s_occupation_3ed671e',
+    'type_of_place_where_the_incident_occurred_bdc967d',
+    'type_of_abuse_431f2ba',
+    'specify_physical_abuse_4867f38',
+    'specify_sexual_abuse_16d36f7',
+    'specify_physical_interaction_d13d273',
+    'sexual_intercourse_a1585d0',
+    'if_no_physical_touch__please_specify_1159dde',
+    'specify_mental_abuse_6182644',
+    'specify_neglect_9b8a22b',
+    'specify_exploitation_ef65c2e',
+    'specify_human_trafficking_dca84dd',
+    'stimulant_a4096c2',
+    'relation_within_family_a6c45fd',
+    'economic_issue_a445217',
+    'physical_issue_2e9f1c6',
+    'mental_issue_4785ece',
+    'urgent_protection_concern',
+    'risk_level',
+    'social_problems_identified_1',
+    'home_visit_required__2',
+    'vagina_details',
+    'cervix_details',
+    'discharge_details',
+    'uterus_details',
+    'penis_details_79f31be',
+    'urethra_and_discharge_60eb64e',
+    'scrotal_sac_f93e04a',
+    'anus_and_rectum_eaf3784',
+    'source_of_information_44cac9a',
+    'disabilities_1',
+    'chronic_illness',
+    'mental_illness',
+    'behavior_of_risk',
+    'developmental_issue',
+    'vaccinations_are_up_to_date',
+    'nutritional_conditions_are_adequate',
+    'caregiver_s_disabilities',
+    'caregiver_s_chronic_illness_es',
+    'caregiver_s_mental_illness_es',
+    'caregiver_s_economic_issue_s',
+    'caregiver_s_substance_abuse_issue',
+    'physical_abuse_issue',
+    'gambling_issue_in_the_family',
+    'substance_abuse_in_the_family_community',
+    'entertainment_venues_near_the_household',
+    'girls_lie_together_in_the_same_room_as_their_male_relatives',
+    'girls_lie_together_in_the_same_room_as_their_female_relatives',
+    'boys_lie_in_the_same_room_as_their_female_relatives',
+    'boys_lie_together_in_the_same_room_as_their_male_relatives',
+    'history_of_illegal_activities_behaviors_in_the_family',
+    'history_of_child_labor_in_the_family_community',
+    'bullying',
+    'mingling_and_causing_annoyance_to_others',
+    'gambling',
+    'substance_abuse',
+    'involved_in_sex_trade',
+    'behaves_in_a_sexually_inappropriate_or_obscene_manner',
+    'resisting_parental_teachings',
+    'family_status',
+    'is_this_person_living_in_the_same_household_as_patient_7d39e1d',
+    'relation',
+    'relation_is_alive',
+    'relation_nationality',
+    'occupation_3',
+    'relation_location_current',
+    'reason_for_unexpected_pregnancy',
+    'specify_contraception_problem',
+    'specify_legal_offenses',
+    'factors_for_pregnancy_termination',
+    'specify_physical_issue',
+    'specify_mental_issue',
+    'specify_family_social_economic_issues',
+    'patient_provided_with_alternative_counseling',
+    'patient_decision_after_being_provided_with_alternative_counseling',
+    'antenatal_care_will_be_provided_at',
+    'support_needed',
+    'support_needed_1',
+    'source_of_information_647b9db',
+    'is_the_subject_given_the_benefit_of_the_doubt_',
+    'assessment_method',
+    'urgent_protection_concern',
+    'protection_concerns',
+    'risk_level',
+    'conference_type',
+    'conference_reason',
+    'conference_case_status',
+    'conference_case_transfer_reason',
+    'relation_to_the_child_37c30dc',
+    'protection_concerns',
+    'sex_check_and_evaluation',
+    'physical',
+    'mental',
+    'social',
+    'physical_1',
+    'mental_1',
+    'reproductive_health',
+    'psycho_social',
+    'status_of_treatment_plan_a8ca0e8',
+    'service_response_type',
+    'service_type',
+    'service_response_timeframe',
+    'service_implementing_agency',
+    'service_implementing_agency_individual',
+    'service_referral',
+    'service_implemented',
+    'service_delivery_location',
+    'status_of_treatment_plan_a8ca0e8',
+    'protection_concerns',
+    'risk_level',
+    'followup_type',
+    'child_was_seen',
+    'reason_child_not_seen',
+    'action_taken_already',
+    'need_follow_up_visit',
+    'recommend_case_closed',
+    'status',
+    'closure_reason',
+  ];
 
-    let option_strings_source = []
+  const externallyDefinedOptionSets = forms
+    .map(form =>
+      form.fields
+        .filter(field => {
+          if (select_fields.includes(field.name)) {
+            return true;
+          } else {
+            console.log(
+              `Error: field ${field.name} not part of specified selectFields`
+            );
+            return false;
+          }
+        })
+        .map(field =>
+          field.hasOwnProperty('option_strings_source')
+            ? {
+                unique_id: field.option_strings_source.replace('lookup ', ''),
+                values: [],
+              }
+            : {
+                unique_id: field.name,
+                values: field.option_strings_text,
+              }
+        )
+    )
+    .flat();
 
-    // Building a new resonpose when missing option_strings_source
-    const build_resp = field => {
-        let newResp = {
-            name: "",
-            option_strings_text: []
-        }
-        if (typeof (field.hasOwnProperty("option_strings_text"))) {
-            newResp["name"] = field.name;
-            newResp["option_strings_text"] = field.option_strings_text;
-        }
+  // Clean up duplicates keys in externallyDefinedOptionSets to get uniqueExternallyDefinedOptionSets
+  let uniqueExternallyDefinedOptionSets = [];
 
-        return newResp;
+  for (i = 0; i < externallyDefinedOptionSets.length; i++) {
+    if (
+      !uniqueExternallyDefinedOptionSets.find(
+        x => x.unique_id === externallyDefinedOptionSets[i].unique_id
+      )
+    ) {
+      uniqueExternallyDefinedOptionSets.push(externallyDefinedOptionSets[i]);
     }
+  }
 
-    // Lookup fn Search for all 139 field listed in fields tab
-    // Search for the field using this path data[*].fields[*].name
-    const lookup = fields => {
-        for (var i = 0, len = fields.length; i < len; i++) {
-
-            if (select_fields.includes(fields[i].name)) {
-                console.log(`Yeey!: field ${fields[i].name} was found in our current select fields`)
-                console.log(typeof (fields[i].option_strings_source))
-
-                option_strings_source.push(
-                    fields[i].hasOwnProperty("option_strings_source") ? fields[i].option_strings_source.replace("lookup ", "") : build_resp(fields[i])
-                )
-            } else {
-                // throw new Error(
-                //     `field ${fields[i].name} not found in our current select fields`
-                // );
-
-                console.log(`Error: field ${fields[i].name} not found in our current select fields`)
-            }
-        }
-
-        return option_strings_source;
-    }
-
-    forms_data.forEach(data => {
-        lookup(data.fields)
-
-    });
-
-    return { ...state, "option_strings_source": option_strings_source };
+  //   console.log(
+  //     'uniqueExternallyDefinedOptionSets',
+  //     uniqueExternallyDefinedOptionSets
+  //   );
+  return { ...state, uniqueExternallyDefinedOptionSets };
 });
 
+// Get _all_ of the actual values for externallyDefinedOptionSets in Primero (they call these "lookups")
+get(`${state.configuration.url}/api/v2/lookups?per=1000000&page=1`, {
+  headers: { 'content-type': 'application/json' },
+  authentication: {
+    username: state.configuration.user,
+    password: state.configuration.password,
+  },
+  strictSSL: false,
+});
 
-// get lookups from Primero
-get(
-    `${state.configuration.url}/api/v2/lookups?per=1000000&page=1`,
-    {
-        headers: { 'content-type': 'application/json' },
-        authentication: {
-            username: state.configuration.user,
-            password: state.configuration.password
-        },
-        strictSSL: false,
-    },
-    state => {
-        return state;
-    }
-);
-
-// Generate a map of option value ids and translation
+// Using the uniqueExternallyDefinedOptionSets, get the option values for each set.
 fn(state => {
-    const lookups_data = state.data.data;
-    const optionStringsSourceLookupName = state.option_strings_source;
+  const optionStringsSourceLookupNames =
+    state.uniqueExternallyDefinedOptionSets;
 
-    const allMappingOutput = [];
-    // Generate the desired mapping output
-    const mappingGenerator = data => {
-        const idsTranslations = [];
-        // Map option values id and translations
-        data.values.forEach((val) => {
-            let { id } = val;
-            let th = val.display_text.th
-            const idThMap = {
-                [id]: th
-            }
+  const lookupTranslations = state.data.data;
 
-            idsTranslations.push(idThMap);
-        });
+  const optionsValuesForStringsSource = lookupTranslations
+    .filter(lookupTranslation => {
+      if (
+        optionStringsSourceLookupNames.includes(lookupTranslation.unique_id)
+      ) {
+        console.log(`I was found ${lookupTranslation.unique_id}`);
+        return true;
+      } else {
+        console.log(
+          `Error: Option ${lookupTranslation.unique_id} was not found in optionStringsSourceLookupNames`
+        );
+        return false;
+      }
+    })
+    .map(lookupTranslation => {
+      console.log(`So let's ${lookupTranslation.unique_id}`);
+      //   const idsTranslations = [];
+      //   //   Map option values id and translations
+      //   lookupTranslation.values.map(val => {
+      //     idsTranslations.push({
+      //       [val.id]: val.display_text.th,
+      //     });
+      //   });
+      //   return idsTranslations;
+    });
 
-        allMappingOutput.push({
-            [data.id]: idsTranslations
-        });
-    }
-    // Options lookup function that check if unique_id === ${optionStringsSourceLookupName}
-    const lookup = data => {
-        for (var i = 0, len = data.length; i < len; i++) {
-
-            if (optionStringsSourceLookupName.includes(data[i].unique_id)) {
-                console.log(`Yeey!: Option was found for lookup ${data[i].unique_id}`)
-                console.log(typeof (data[i].unique_id))
-
-                mappingGenerator(data[i]);
-
-            } else {
-                // throw new Error(
-                //     `Error: Option ${data[i].unique_id} was not found in optionStringsSourceLookupName`
-                // );
-
-                console.log(`Error: Option ${data[i].unique_id} was not found in optionStringsSourceLookupName`)
-            }
-        }
-
-        return allMappingOutput;
-    }
-
-    let desiredMapOutput = lookup(lookups_data)
-
-    return { desiredMapOutput }
+  return { ...state, optionsValuesForStringsSource };
 });
