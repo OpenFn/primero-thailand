6998c1f44c3d78c71d5c522f81d648d8161ec78e
Aleksa Krolls <aleksagk@gmail.com>
2022-09-23 12:06:12 +0300

    aleksa@openfn.org's change on OpenFn.org

--- jobs/child-shield/syncInterventionsToChildShield.js
+++ jobs/child-shield/syncInterventionsToChildShield.js
@@ -1,5 +1,5 @@
 //To update when spec for Flow 1 , job #3 submitted
 fn(state => {
-  console.log('final state of Job2::', state); 
+  console.log('final state of Job2::', state.data); 
   return state; 
 })
