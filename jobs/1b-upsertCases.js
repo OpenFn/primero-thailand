81eb3f86a64dd6639a8b4eeaa834ff45413d7e29
mtuchi <mtuchidev@gmail.com>
2023-01-11 16:39:29 +0300

    add log for upserting cases

--- jobs/1b-upsertCases.js
+++ jobs/1b-upsertCases.js
@@ -3612,7 +3612,7 @@ each(
       ...flattenMappingForRiskModel,
     };
 
-    // console.log('Upserting case', JSON.stringify(extendedCaseDetails, null, 2));
+    console.log('Upserting case', JSON.stringify(extendedCaseDetails, null, 2));
     // return { ...state, extendedCaseDetails };
     return upsertCase(
       {
