20b6ee3f3f9a5f389b4c3f759a4f204c28ac28c0
jayesse <97878823+jayesse@users.noreply.github.com>
2023-01-25 15:19:03 +0000

    Update 1b-upsertCases.js
    Put CQ1 before CQ2 for Week 1 main question 1

--- jobs/1b-upsertCases.js
+++ jobs/1b-upsertCases.js
@@ -971,18 +971,17 @@ each(
         source: [
           {
             type: 'select',
+            questionnaire_code: 'CQ1',
             week: 1,
+            value: '1. ฉันชอบตัวเอง',
+          },
+          {
+            type: 'select',
             questionnaire_code: 'AUQUEI_CQ2',
             groupDescription:
               'ส่วนที่ 2: แบบสอบถามคุณภาพชีวิตฉบับเพิ่มเติม (CQ2) จำนวน 7 ข้อ',
             week: 1,
             value: '1. เธอชอบตัวเอง',
-          },
-          {
-            type: 'select',
-            questionnaire_code: 'CQ1',
-            week: 1,
-            value: '1. ฉันชอบตัวเอง',
           },
         ],
 
